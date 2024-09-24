d42626477f9423cdad039c7895f82f48a3750327
Abhishek Patil <abishkpatil@gmail.com>
2024-09-24 19:52:11 +0530

    feat: update demo video

--- src/components/DemoVideo.jsx
+++ src/components/DemoVideo.jsx
@@ -1,6 +1,6 @@
 const DemoVideo = () => {
     return <div>
-        <iframe className="m-auto md:min-w-[854px] md:min-h-[480px]" src="https://www.loom.com/embed/c1ff7adf304d47259f71fae09e5f738c?sid=8faf168d-9647-4ad8-b103-0acc55feee9e" allow="autoplay"></iframe>
+        <iframe className="m-auto md:min-w-[854px] md:min-h-[480px]" src="https://drive.google.com/file/d/1hQSjQc0GdQj8kPrrIlqLJvPzUpphU5hc/preview" allow="autoplay"></iframe>
     </div>
 }
 
